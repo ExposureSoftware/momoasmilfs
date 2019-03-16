74a609cbe486f481c81e66b9f7cb82ebc0580a29
Marshall Davis <toothlessrebel@gmail.com>
2019-03-16 01:47:49 -0400

    Configure for production assets and mail.

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,14 @@ module MomoasmilfsClub
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['MAIL_USER'],
+      :password => ENV['MAIL_PASSWORD'],
+      :domain => 'momoasmilfs.club',
+      :address => ENV['MAIL_SERVER'],
+      :port => ENV['MAIL_PORT'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
